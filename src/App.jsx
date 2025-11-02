a763389e806d1c53b1ca596510cb024543c2242a
rohishidayah-pixel <rohishidayah@gmail.com>
2025-11-02 07:05:27 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,26 @@ import Topbar from "./components/Topbar";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
+import NewsDetail from "./pages/NewsDetail"; 
 
 export default function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-[#f2f4f7] dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors">
         
-        {/* 🔹 Topbar di atas */}
+  
         <Topbar />
 
-        {/* Konten utama */}
+    
         <main className="flex-grow container mx-auto px-4 py-6 pb-24">
           <Routes>
+          
             <Route path="/" element={<Home />} />
+
+    
             <Route path="/login" element={<Login />} />
+
+         
             <Route
               path="/dashboard"
               element={
@@ -30,6 +36,8 @@ export default function App() {
                 </ProtectedRoute>
               }
             />
+
+           
             <Route
               path="/dashboard/newsform"
               element={
@@ -38,13 +46,16 @@ export default function App() {
                 </ProtectedRoute>
               }
             />
+
+           
+            <Route path="/berita/:id" element={<NewsDetail />} />
           </Routes>
         </main>
 
-        {/* Footer desktop */}
+       
         <Footer />
 
-        {/* 🔹 Navbar bawah untuk mobile */}
+       
         <Navbar />
       </div>
     </Router>
